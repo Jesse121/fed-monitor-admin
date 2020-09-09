7d16bb8aa1b508ba5bd586fcc2264f646580245e
yangdong <yangdong@guotailimin.com>
2020-09-09 11:21:14 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import Vue from "vue";
+// import "normalize.css";
+import ElementUI from "element-ui";
+
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+
+Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
